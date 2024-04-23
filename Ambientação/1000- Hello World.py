c60173176e3fc79c41fc0d76382f2efb1d5cae34
João Victor Vaz <164522321+vaz-joao@users.noreply.github.com>
2024-04-23 18:18:25 -0300

    Exercicios iniciais

---
+++ Ambientação/1000- Hello World.py
@@ -0,0 +1,17 @@
+'''
+Bem-vindo ao beecrowd!
+
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". 
+Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+Entrada
+Este problema não possui nenhuma entrada.
+
+Saída
+Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
+'''
+
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Hello WOrld!")
+Hello WOrld!
